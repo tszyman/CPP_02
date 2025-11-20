380bc1a596b0d7db8c05ade895d51426d81a88a9
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-11-20 21:00:44 +0100

    [ex00] Copmpleted

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed() : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->_value = other.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+}
