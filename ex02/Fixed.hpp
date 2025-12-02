9d55ed38df5e9b3ecf5ab917be717248669f7237
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-12-02 18:34:50 +0100

    [ex02] Declarations of the functions added

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -19,6 +19,32 @@ public:
 	float				toFloat(void) const;
 	int					toInt(void) const;
 
+	/* Comparison operators */
+	bool operator>(Fixed const &other) const;
+	bool operator<(Fixed const &other) const;
+	bool operator>=(Fixed const &other) const;
+	bool operator<=(Fixed const &other) const;
+	bool operator==(Fixed const &other) const;
+	bool operator!=(Fixed const &other) const;
+
+	/* Arithmetic operators */
+	Fixed operator+(Fixed const &other) const;
+	Fixed operator-(Fixed const &other) const;
+	Fixed operator*(Fixed const &other) const;
+	Fixed operator/(Fixed const &other) const;
+
+	/* Increment/decrement */
+	Fixed &operator++();
+	Fixed operator++(int);
+	Fixed &operator--();
+	Fixed operator--(int);
+
+	/* min/max */
+	static Fixed 		&min(Fixed &a, Fixed &b);
+	static Fixed const	&min(Fixed const &a, Fixed const &b);
+	static Fixed		&max(Fixed &a, Fixed &b);
+	static Fixed const	&max(Fixed const &a, Fixed const &b);
+
 	private:
 	int					_value;
 	static const int	_fractionalBits;
